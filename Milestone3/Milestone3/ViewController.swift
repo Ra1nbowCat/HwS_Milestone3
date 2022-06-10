aaec2bfd87b28210a1115edc0c59b0cef9f532ac
Lekhov Ilya <solwanlomtohs@mail.ru>
2022-06-10 10:03:03 +0300

    Added guess TextField

--- Milestone3/Milestone3/ViewController.swift
+++ Milestone3/Milestone3/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     
     let wordTextField = UITextField()
+    let guessTextField = UITextField()
     var randomWord = ["Milk", "Coconut", "Computer", "Language", "Book"]
 
     override func viewDidLoad() {
@@ -22,22 +23,32 @@ class ViewController: UIViewController {
     
     func addSubviews() {
         view.addSubview(wordTextField)
+        view.addSubview(guessTextField)
     }
     
     func addWordToTextField() {
-        wordTextField.text = randomWord.randomElement()
+        wordTextField.text = randomWord.randomElement()?.lowercased()
     }
 
     func subviewsSettings() {
         wordTextField.translatesAutoresizingMaskIntoConstraints = false
         wordTextField.isUserInteractionEnabled = false
         wordTextField.font = UIFont.systemFont(ofSize: 50)
+        
+        guessTextField.translatesAutoresizingMaskIntoConstraints = false
+        guessTextField.font = UIFont.systemFont(ofSize: 35)
+        guessTextField.layer.borderColor = UIColor.lightGray.cgColor
+        guessTextField.layer.borderWidth = 1
     }
     
     func makeConstraints() {
         NSLayoutConstraint.activate([
             wordTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            wordTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            wordTextField.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            guessTextField.centerYAnchor.constraint(equalTo: wordTextField.centerYAnchor, constant: view.frame.height / 4),
+            guessTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            guessTextField.widthAnchor.constraint(equalToConstant: view.frame.width / 1.5)
         ])
     }
 
